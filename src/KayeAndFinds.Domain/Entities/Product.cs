825d6dcd6be9a72f7d039c4b3ffbbb297cd6f005
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-11 07:36:11 +0800

    feat: specify basic product fields

---
+++ src/KayeAndFinds.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using KayeAndFinds.Domain.Enums;
+
+namespace KayeAndFinds.Domain.Entities;
+
+public class Product
+{
+    public string Name { get; set; } = default!;
+    public ProductCategory ProductCategory { get; set; } = ProductCategory.Clothing;
+    public string Brand { get; set; } = default!;
+    public string Condition { get; set; } = default!;
+    public string Size { get; set; } = default!;
+    public string Color { get; set; } = default!;
+}
