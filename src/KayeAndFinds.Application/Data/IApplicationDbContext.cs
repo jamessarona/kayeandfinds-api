f3edc2454612be898dbb706ded42f903f28ac79f
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-11 22:24:27 +0800

    feat: add Db Context for Products

---
+++ src/KayeAndFinds.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace KayeAndFinds.Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Product> Products { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+}
