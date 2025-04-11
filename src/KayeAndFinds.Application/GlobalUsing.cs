63008c4a31be10f170c3b119040cb85bc42bf483
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-11 22:36:07 +0800

    feat: register dependency

---
+++ src/KayeAndFinds.Application/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using KayeAndFinds.Application.Data;
+global using KayeAndFinds.Application.DTOs;
+global using KayeAndFinds.Common.CQRS;
+global using KayeAndFinds.Common.Pagination;
+global using KayeAndFinds.Domain.Entities;
+global using KayeAndFinds.Domain.Enums;
+global using Marten;
+global using Microsoft.EntityFrameworkCore;
---
+++ src/KayeAndFinds.Application/Services/Products/Queries/GetProductsHandler.cs
@@ -0,0 +1,12 @@
+﻿using KayeAndFinds.Application.Extensions;
+
+namespace KayeAndFinds.Application.Services.Products.Queries;
+
+public class GetProductsHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
