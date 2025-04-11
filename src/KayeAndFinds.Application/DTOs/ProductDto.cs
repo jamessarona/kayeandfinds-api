7dbaec38b2b7fabcb16c6e5e24c4f86db38a874c
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-11 22:25:21 +0800

    feat: add product dto

---
+++ src/KayeAndFinds.Application/DTOs/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace KayeAndFinds.Application.DTOs;
+
+public record ProductDto(
+    Guid Id,
+    string Name,
+    ProductCategory ProductCategory,
+    string Brand,
+    string Condition,
+    string Size,
+    string Color);
---
+++ src/KayeAndFinds.Application/Extensions/ProductExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace KayeAndFinds.Application.Extensions;
+
+public static class ProductExtensions
+{
+    public static IEnumerable<ProductDto> ToProductDtoList(this IEnumerable<Product> products)
+    {
+        return products.Select(product => new ProductDto(
+            Id: product.Id,
+            Name: product.Name,
+            ProductCategory: product.ProductCategory,
+            Brand: product.Brand,
+            Condition: product.Condition,
+            Size: product.Size,
+            Color: product.Color
+            ));
+    }
+
+    public static ProductDto ToProductDto(this Product product)
+    {
+        return DtoFromProduct(product);
+    }
+
+    private static ProductDto DtoFromProduct(Product product)
+    {
+        return new ProductDto(
+            Id: product.Id,
+            Name: product.Name,
+            ProductCategory: product.ProductCategory,
+            Brand: product.Brand,
+            Condition: product.Condition,
+            Size: product.Size,
+            Color: product.Color
+         );
+    }
+}
