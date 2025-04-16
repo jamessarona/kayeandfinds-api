05c0c8bf7467700b72f38ac75c57f76a01a2e0eb
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-16 17:10:16 +0800

    feat: add create product command

--- src/KayeAndFinds.Application/Services/Products/Commands/CreateProduct/CreateProductCommand.cs
+++ src/KayeAndFinds.Application/Services/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -1,5 +1,21 @@
-﻿namespace KayeAndFinds.Application.Services.Products.Commands.CreateProduct;
+﻿using FluentValidation;
 
-class CreateProductCommand
+namespace KayeAndFinds.Application.Services.Products.Commands.CreateProduct;
+
+public record CreateProductCommand(ProductDto Product)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
 {
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Product.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Product.ProductCategory).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.Product.Brand).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Product.Condition).NotEmpty().WithMessage("Condition is required");
+        RuleFor(x => x.Product.Size).NotEmpty().WithMessage("Size is required");
+        RuleFor(x => x.Product.Condition).NotEmpty().WithMessage("Color is required");
+    }
 }
