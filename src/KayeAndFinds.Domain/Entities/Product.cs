8c24f4f355651b3ee9d704f27f2481c85cfcd833
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-16 17:10:51 +0800

    feat: create mapping of DTO to model

--- src/KayeAndFinds.Domain/Entities/Product.cs
+++ src/KayeAndFinds.Domain/Entities/Product.cs
@@ -8,4 +8,21 @@ public class Product : Entity<Guid>
     public string Condition { get; set; } = default!;
     public string Size { get; set; } = default!;
     public string Color { get; set; } = default!;
+
+    public static Product Create(Guid id, string name, ProductCategory productCategory,
+        string brand, string condition, string size, string color)
+    {
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            ProductCategory = productCategory,
+            Brand = brand,
+            Condition = condition,
+            Size = size,
+            Color = color
+        };
+
+        return product;
+    }
 }
