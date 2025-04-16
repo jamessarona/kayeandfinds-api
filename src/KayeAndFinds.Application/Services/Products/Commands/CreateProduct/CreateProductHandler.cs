ce1ca686e61b633bd901dc5a2c79b8df30694cec
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-16 17:10:36 +0800

    feat: create product handler

--- src/KayeAndFinds.Application/Services/Products/Commands/CreateProduct/CreateProductHandler.cs
+++ src/KayeAndFinds.Application/Services/Products/Commands/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,32 @@
-﻿namespace KayeAndFinds.Application.Services.Products.Commands.CreateProduct;
+﻿
+namespace KayeAndFinds.Application.Services.Products.Commands.CreateProduct;
 
-class CreateProductHandler
+public class CreateProductHandler
+    (IApplicationDbContext dbContext)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = CreateNewProduct(command.Product);
+
+        dbContext.Products.Add(product);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+
+    private Product CreateNewProduct(ProductDto productDto)
+    {
+        var newProduct = Product.Create(
+            id: Guid.NewGuid(),
+            name: productDto.Name,
+            productCategory: productDto.ProductCategory,
+            brand: productDto.Brand,
+            condition: productDto.Condition,
+            size: productDto.Size,
+            color: productDto.Color
+        );
+
+        return newProduct;
+    }
 }
